fe5314b75b5a1a27f4e1479caf912df4bdddff26
Adesina Opeoluwa Martins <opehardesina@gmail.com>
2020-02-09 20:51:58 +0100

    Updates

---
+++ precache-manifest.eefd3bca7793d78a9442bf89a557ef8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b10be4d40326eac9392f3ebec7ea206",
+    "url": "/Todo-List-React-/index.html"
+  },
+  {
+    "revision": "d391298e1fa1d72d2c4c",
+    "url": "/Todo-List-React-/static/css/main.17f52fd0.chunk.css"
+  },
+  {
+    "revision": "fb543156129bc5558358",
+    "url": "/Todo-List-React-/static/js/2.23919358.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-List-React-/static/js/2.23919358.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d391298e1fa1d72d2c4c",
+    "url": "/Todo-List-React-/static/js/main.530a439b.chunk.js"
+  },
+  {
+    "revision": "f896a074e70b23c86ba9",
+    "url": "/Todo-List-React-/static/js/runtime-main.18f56537.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todo-List-React-/static/media/logo.5d5d9eef.svg"
+  }
+]);
